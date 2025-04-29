f0bb3bba623623dad84cf9f0540cd4d06c195700
Ahmed Emam <88343933+ahmedemam2@users.noreply.github.com>
2025-04-29 15:09:08 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,19 +4,19 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import shutil
+    
 
 major = "Computer Science"
+
 print("Choose degree:")
 print("1 = Bachelor")
 print("2 = Master")
 print("3 = PhD")
 print("'' = All degrees")
-Bachelor = 1
-Master = 2
-PhD = 3
+degree_input = 2
 All = ""
-
 format_major = lambda x: x.replace(" ", "%20")
+
 
 def get_number_of_programs(page):
     html = page.content()
@@ -31,7 +31,7 @@ def get_number_of_programs(page):
     return None  
 
 
-base_url = f"https://www2.daad.de/deutschland/studienangebote/international-programmes/en/result/?q={format_major(major)}&fos=&cert=&admReq=&langExamPC=&scholarshipLC=&langExamLC=&scholarshipSC=&langExamSC=&degree%5B%5D={Master}&langDeAvailable=&langEnAvailable=&lang%5B%5D=&modStd%5B%5D=&cit%5B%5D=&tyi%5B%5D=&ins%5B%5D=&fee=&bgn%5B%5D=&dat%5B%5D=&prep_subj%5B%5D=&prep_degree%5B%5D=&sort=4&dur=&subjects%5B%5D=&limit=10&offset=&display=list"
+base_url = f"https://www2.daad.de/deutschland/studienangebote/international-programmes/en/result/?q={format_major(major)}&fos=&cert=&admReq=&langExamPC=&scholarshipLC=&langExamLC=&scholarshipSC=&langExamSC=&degree%5B%5D={degree_input}&langDeAvailable=&langEnAvailable=&lang%5B%5D=&modStd%5B%5D=&cit%5B%5D=&tyi%5B%5D=&ins%5B%5D=&fee=&bgn%5B%5D=&dat%5B%5D=&prep_subj%5B%5D=&prep_degree%5B%5D=&sort=4&dur=&subjects%5B%5D=&limit=10&offset=&display=list"
 programs_list = []
 program_details = []
 
@@ -45,6 +45,7 @@ with sync_playwright() as p:
         page.wait_for_selector("h2.c-result-header__title")
 
         number_of_programs = get_number_of_programs(page)
+        
         if number_of_programs:
             print(f"Number of programs found: {number_of_programs}")
             json_data_url = f"https://www2.daad.de/deutschland/studienangebote/international-programmes/api/solr/en/search.json?cert=&admReq=&langExamPC=&scholarshipLC=&langExamLC=&scholarshipSC=&langExamSC=&fos=&langDeAvailable=&langEnAvailable=&fee=&sort=4&dur=&q={format_major(major)}&limit={number_of_programs}&offset=&display=list&isElearning=&isSep="
@@ -61,15 +62,23 @@ with sync_playwright() as p:
         for id, uni in programs_list:
             print(id, uni)
             try:
-                page.goto(f"https://www2.daad.de/deutschland/studienangebote/international-programmes/en/detail/{id}/", timeout=5000)
+                degree_url = f"https://www2.daad.de/deutschland/studienangebote/international-programmes/en/detail/{id}/"
+                print(degree_url)
+                page.goto(degree_url, timeout=5000)
                 page.wait_for_load_state("networkidle", timeout=15000)
+
+                degree_name = page.locator('span.d-sm-none').text_content().strip()
+
                 page.click("#costs-tab", timeout=5000)
                 funding_label = page.locator("dt:has-text('Funding opportunities within the university')").nth(0)
                 funding_value = funding_label.locator("xpath=following-sibling::dd[1]").inner_text().strip()
+
                 program_details.append({
                     "Program ID": id,
                     "University": uni,
-                    "Funding Opportunities": funding_value
+                    "degree": degree_name,
+                    "Funding Opportunities": funding_value,
+                    "link": degree_url
                 })            
             except PlaywrightTimeoutError:
                 print(f"Timeout while processing program {id} - {uni}, skipping...")
@@ -85,11 +94,10 @@ if program_details:
     keys = program_details[0].keys()
     
     filename = f"daad_programs_{major.replace(' ', '_')}.csv"
-    
+        
     with open(filename, "w", newline="", encoding="utf-8") as f:
         dict_writer = csv.DictWriter(f, fieldnames=keys)
         dict_writer.writeheader()
         dict_writer.writerows(program_details)
-shutil.copy(filename, "output.csv")
-
-    
+        
+    shutil.copy(filename, "output.csv")
